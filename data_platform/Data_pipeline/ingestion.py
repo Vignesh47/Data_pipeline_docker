3c1e3dfc64c33b6353ce8da21ce32b59d9d46ba5
Vignesh47 <techievignesh47@gmail.com>
2024-03-17 18:01:44 +0000

    Updated Requirement file

--- data_platform/Data_pipeline/ingestion.py
+++ data_platform/Data_pipeline/ingestion.py
@@ -1,7 +1,6 @@
 #importing required Libraries for data ingestion
 
-
-import pyspark
+#import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, udf
 from pyspark.sql.functions import *
@@ -15,32 +14,24 @@ class Ingest:
     def __init__(self,spark):
         self.spark = spark
         # Creating Object for Spark function
-        spark = SparkSession.builder.appName("My new app").enableHiveSupport().getOrCreate()
+        spark = SparkSession.builder.appName("Data Transformation").enableHiveSupport().getOrCreate()
         logging.info("Spark has been initiated.......")
 
 
     def ingest_data(self):
         try:
             logger = logging.getLogger("Ingest")
+            base_data_path = os.path.join(os.getcwd(),"data_platform", "data", "Base_data.xlsx")
+            vechile_data = os.path.join(os.getcwd(),"data_platform", "data", "vehicle_line_mapping.csv")
+            options_data = os.path.join(os.getcwd(), "data_platform", "data", "options_data.csv")
 
-            # define the schema for the JSON data
-            schema = StructType([
-                StructField("abstract", StructType([
-                    StructField("_value", StringType())
-                ])),
-                StructField("label", StructType([
-                    StructField("_value", StringType())
-                ])),
-                StructField("numberOfSignatures", LongType())
-            ])
+            # read the CSV file into a DataFramE
+            B_df = self.spark.read.format("com.crealytics.spark.excel").option("header", "true") \
+                        .option("inferSchema", "true").load(base_data_path)
+            vechile_df = self.spark.read.csv(vechile_data,header=True)
+            options_df =self.spark.read.csv(options_data,header=True)
 
-            logging.info("Schema has been  Created")
-
-            # read the JSON file into a DataFrame
-
-            input_data_path = os.path.join(os.getcwd(), "data", "input_data.json")
-            petition_df = self.spark.read.json(input_data_path, schema)
-            print(petition_df)
+            print(vechile_df,options_df)
 
             logging.info("File has been Loaded successfully")
         except Exception as exp:
@@ -48,5 +39,5 @@ class Ingest:
             logging.error("Ingestion Process has been failed.>" + str(exp))
             raise Exception(" File is not available in the input path ")
 
-        return petition_df
+        return B_df,vechile_df,options_df
 
--- data_platform/main.py
+++ data_platform/main.py
@@ -6,6 +6,7 @@ from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 import logging
 import logging.config
+import os
 
 
 #importing ingestion, Transformation and Storage file
@@ -14,18 +15,18 @@ from testing import test_ingestion
 
 
 class data_pipeline:
-    logging.config.fileConfig("/Users/darkstorm/PycharmProjects/Mydatapipeline/Configs/logging.config")
+    logging.config.fileConfig("/Users/vigneshnandhan/Dataflow/Data_pipeline_docker/data_platform/Configs/logging.config")
     def pipeline(self):
         try:
             logging.info("Pipeline has been Initiated successfully")
             ingest = ingestion.Ingest(self.spark)
             raw_data = ingest.ingest_data()
             logging.info("File ingestion has been successfully completed")
-            transform = transformation.Transform(self.spark)
-            base_data = transform.transform_data(raw_data)
+            #transform = transformation.Transform(self.spark)
+            #base_data = transform.transform_data(raw_data)
             logging.info("Base --->>>> Data has been successfully cleaned")
-            store = storage.Storage(self.spark)
-            store.storage_data(base_data)
+            #store = storage.Storage(self.spark)
+            #store.storage_data(base_data)
             logging.info("Integration--->>>> Data has been stored successfully")
         except Exception as exp:
             logging.error("Pipeline failed due to >"+str(exp))
@@ -33,7 +34,7 @@ class data_pipeline:
 
 
     def spark_build(self):
-        self.spark = SparkSession.builder.appName("Aviva_Petition_Data").getOrCreate()
+        self.spark = SparkSession.builder.appName("JLR_Data").config("spark.jars.packages", "com.crealytics:spark-excel_2.12:0.14.0").getOrCreate()
 
 if __name__ == "__main__":
     pipeline_new = data_pipeline()
